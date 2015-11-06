f94b42f4676f24b09238cfa41eb372e4c3d31df1
Veronica Paulsen <veronicapaulsen@me.com>
2015-11-06 10:46:35 -0700

    backtracking works yaaaaaaay

--- FourColors.java
+++ FourColors.java
@@ -47,76 +47,85 @@ class Edge{
 
 class Graph{
 
-    int[] colors = {0, 1, 2, 3};
     ArrayList<Vertex> vertices = new ArrayList<>();
 
     public Graph( ArrayList<Vertex> _vertices){
 	vertices = _vertices;
     }
 
-    public void colorVertex(Vertex v){
-
+    public void colorVertex(Vertex v, int[] colors){
+		
 	ArrayList<Integer> unAvailableColors = new ArrayList<>();
 	int c = -1;
 
-	if(v.color == -1){
-	    for(Edge e : v.edges){
-		//		    System.out.println(v.name + " is attached to " + e.end.color);
-		for(int i=0; i < colors.length; i++){
-		    if(e.end.color == colors[i] && !unAvailableColors.contains(colors[i])){			    
-			unAvailableColors.add(colors[i]);
-		    }
+	for(Edge e : v.edges){
+	    //		    System.out.println(v.name + " is attached to " + e.end.color);
+	    for(int i=0; i < colors.length; i++){
+		if(e.end.color == colors[i] && !unAvailableColors.contains(colors[i])){			    
+		    unAvailableColors.add(colors[i]);
 		}
-	    }		
-	    if(unAvailableColors.size() < 4){
-		for(int i=0; i < colors.length; i++){
-		    if(!unAvailableColors.contains(colors[i])){
-			c = colors[i];
-		    }
-		}		    
-		v.color = c;
 	    }
 	}		
+	if(unAvailableColors.size() < colors.length){
+	    for(int i=0; i < colors.length; i++){
+		if(!unAvailableColors.contains(colors[i])){
+		    c = colors[i];
+		}
+	    }		    
+	    v.color = c;
+	}
     }
 
     public void colorGraph(){
 
+	int[] colors = {0, 1, 2, 3};
 	this.vertices.get(0).color = 0;
 
-
-
 	for(Vertex v : this.vertices){
-	    colorVertex(v);
+	    colorVertex(v, colors);
 	}
 
 	for(Vertex v : this.vertices){
 	    if(v.color == -1){
-		//KEMP CHAINS OR BACKTRACK
+		backTrack(v);
 	    }
 	}
     }
 
     public void backTrack(Vertex v){
 	int min_connected = v.edges.get(0).end.edges.size();
-	Vertex min_vertex;
+	Vertex min_vertex = v.edges.get(0).end;
 
+	//find the least connected vertex that is connected to v
 	for(Edge e : v.edges){
-	    if(e.end.edges.size() < min_connected){
+	    if(e.end.edges.size() < min_connected && e.end.color != -1){
 		min_connected = e.end.edges.size();
 		min_vertex = e.end;
 	    }
 	}	
+	System.out.println("min_v: " + min_vertex.name);
+	int[] original_colors = {0,1,2,3};
+	int[] colors = new int[3];
+	int count =0;
+	//create color array without the current color of v
+	for(int i=0; i < original_colors.length; i++){
+	    if( i != min_vertex.color){
+		colors[count] = i;
+		System.out.print("c " + count + ": " +colors[count]);
+		count++;
+	    }
+	}
+	System.out.println();
+	//recolor this least connected vertex NOT THE SAME COLOR
+	colorVertex(min_vertex, colors);
 
+	colorVertex(v, original_colors);
     }
 
 }
 
 
 public class FourColors{
-
-    /*    public Graph kemp(Vertex v){
-	for(Edge e : v.edges
-	}*/
 
     public static void main(String[] args){
 
@@ -125,66 +134,52 @@ public class FourColors{
 	Vertex C = new Vertex("C");
 	Vertex D = new Vertex("D");
 	Vertex E = new Vertex("E");
-	Vertex F = new Vertex("F");
 
 	Edge ab = new Edge(A, B);
-	Edge ac = new Edge(A, C);
 	Edge ad = new Edge(A, D);
 	Edge ae = new Edge(A, E);
-	Edge af = new Edge(A, F);
 
 	Edge ba = new Edge(B, A);
 	Edge bc = new Edge(B, C);
+	Edge be = new Edge(B, E);
+	Edge bd = new Edge(B, D);
 
-	Edge ca = new Edge(C, A);
 	Edge cb = new Edge(C, B);
 	Edge cd = new Edge(C, D);
-	Edge cf = new Edge(C, F);
+	Edge ce = new Edge(C, E);
 
 	Edge de = new Edge(D, E);
 	Edge dc = new Edge(D, C);
 	Edge da = new Edge(D, A);
-	Edge df = new Edge(D, F);
+	Edge db = new Edge(D, B);
 
 	Edge ea = new Edge(E, A);
+	Edge eb = new Edge(E, B);
+	Edge ec = new Edge(E, C);
 	Edge ed = new Edge(E, D);
-	Edge ef = new Edge(E, F);
-
-	Edge fa = new Edge(F, A);
-	Edge fe = new Edge(F, E);
-	Edge fc = new Edge(F, C);
-	Edge fd = new Edge(F, D);
-
-
 
 	A.edges.add(ab);
 	A.edges.add(ad);
-	A.edges.add(ac);
 	A.edges.add(ae);
-	A.edges.add(af);	
 	
-
 	B.edges.add(ba);
 	B.edges.add(bc);
+	B.edges.add(be);
+	B.edges.add(bd);
 
 	C.edges.add(cb);
 	C.edges.add(cd);
-	C.edges.add(ca);
-	C.edges.add(cf);
+	C.edges.add(ce);
 
 	D.edges.add(de);
 	D.edges.add(da);
 	D.edges.add(dc);
-	D.edges.add(df);
+	D.edges.add(db);
 
 	E.edges.add(ed);
 	E.edges.add(ea);
-	E.edges.add(ef);
-
-	F.edges.add(fa);
-	F.edges.add(fe);
-	F.edges.add(fc);
-	F.edges.add(fd);
+	E.edges.add(eb);
+	E.edges.add(ec);
 
 	ArrayList<Vertex> vertices = new ArrayList<>();
 	vertices.add(A);
@@ -192,7 +187,6 @@ public class FourColors{
 	vertices.add(C);
 	vertices.add(D);
 	vertices.add(E);	
-	vertices.add(F);
 
 	Graph g = new Graph(vertices);
 
